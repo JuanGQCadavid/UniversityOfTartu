95ec946abefaed101c01d6d55562679cfe85523f
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2024-09-13 11:30:44 +0300

    Cleaning

--- Algorithmics/hw2/devops/start.sh
+++
@@ -1,5 +0,0 @@
-export INFLUX_HOST_ADD=http://172.17.91.7:8086
-export INFLUX_ORG=UT
-export INFLUX_TOKEN=YYmgQ7vp6l2dzJOB-oP8SoijWdpxQpvq8LpQI1cM1YRKnO6KnggeTmPm9PXuPmastJ_S3_m_zoEk1Ff2nDubuA==
-
-python3 app.py
