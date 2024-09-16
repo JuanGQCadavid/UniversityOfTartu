d9114c4361de85e45a845f8995935c9e486769fd
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2024-09-16 18:35:05 +0300

    Keeps failing

--- Algorithmics/hw2/cmd/ex3/main.py
+++ Algorithmics/hw2/cmd/ex3/main.py
@@ -5,21 +5,73 @@ import sys
 sys.setrecursionlimit(1500)
 
 # Generate a list of random numbers
-rnumbers = random.sample(range(1, 10**8), 10**6)
-# rnumbers = random.sample(range(1, 10**8), 10**3) # small data for initial test
+# rnumbers = random.sample(range(1, 10**8), 10**6)
+rnumbers = random.sample(range(1, 10**2), 10**1) # small data for initial test
 snumbers = rnumbers.copy()
 snumbers.sort()
 
-# Sort the list using the quick sort algorithm
+def timeMesure(testName, funct):
+   print(testName)
+   for i in range(3):
+    numbers = rnumbers.copy()
+    start_time = time.time()
+    funct( numbers, 0, len(numbers)-1 )
+    end_time = time.time()
+    print(f"Time to sort {len(numbers)} elements: {end_time - start_time:.2f} seconds")
+    if numbers == snumbers:
+        pass
+        print("OK - was correct")
+    else:
+        print("Error: code was wrong")
+        print(numbers)
 
-def quickSortVStack(arr):
+def quickSortV3VStack(arr, low, high):
     operations = []
 
     operations.append({
        "low": 0, 
-       "high": len(numbers)-1 
+       "high": len(arr)-1 
     })
+    ## TO HERE
 
+    while True:
+        if len(operations) == 0:
+            break
+
+        op = operations.pop()
+        # print(op)
+
+        if (op["high"] <= op["low"]) : continue
+
+        pivot = arr[op["high"]]     # pivot
+        green = op["low"] -1
+        yellow = op["low"]-1
+
+        while( green < op["high"] and yellow < op["high"]) :
+            green = green+1
+            if arr[green] <= pivot:
+                yellow = yellow + 1
+                if green > yellow :
+                    arr[yellow], arr[green] = arr[green], arr[yellow]
+
+        pi = yellow
+        operations.append({
+            "low": pi+1, 
+            "high": op["high"]
+        })
+
+        operations.append({
+            "low": op["low"], 
+            "high": pi-1
+        })
+
+def quickSortVStack(arr, low, high):
+    operations = []
+
+    operations.append({
+       "low": 0, 
+       "high": len(arr)-1 
+    })
 
     while True:
         if len(operations) == 0:
@@ -77,8 +129,6 @@ def quickSortV3(arr, low, high):
 # arr = [3,2,5,0,1,8,7,6,9,4]
 # quickSortV3(arr=arr, low=0, high=len(arr)-1)
 # print(arr)
-
-
 def quickSort(arr, low, high):
     if (high <= low) : return
 
@@ -98,51 +148,45 @@ def quickSort(arr, low, high):
     quickSort(arr, low, pi-1)
     quickSort(arr, pi+1, high)
 
-print( "# Quicksort V3. Recursived")
-for i in range(3):
-  numbers = rnumbers.copy()
-  start_time = time.time()
-  quickSortV3( numbers, 0, len(numbers)-1 )
-  end_time = time.time()
-  print(f"Time to sort {len(numbers)} elements: {end_time - start_time:.2f} seconds")
-  if numbers == snumbers:
-    pass
-    print("OK - Quicksort was correct")
-  else:
-    print("Error: Quicksort code was wrong")
+def median_of_three(arr, low, high):
+    mid = (high - low) // 2
 
-print( "# Quicksort V. stacks")
-for i in range(3):
-  numbers = rnumbers.copy()
-  start_time = time.time()
-  quickSortVStack( numbers)
-  end_time = time.time()
-  print(f"Time to sort {len(numbers)} elements: {end_time - start_time:.2f} seconds")
-  if numbers == snumbers:
-    pass
-    print("OK - Quicksort was correct")
-  else:
-    print("Error: Quicksort code was wrong")
+    if arr[low] > arr[mid]:
+        arr[low], arr[mid] = arr[mid], arr[low]
 
-print( "# Quicksort original")
-for i in range(3):
-  numbers = rnumbers.copy()
-  start_time = time.time()
-  quickSort( numbers, 0, len(numbers)-1 )
-  end_time = time.time()
-  print(f"Time to sort {len(numbers)} elements: {end_time - start_time:.2f} seconds")
-  if numbers == snumbers:
-    pass
-    print("OK - Quicksort was correct")
-  else:
-    print("Error: Quicksort code was wrong")
+    if arr[low] > arr[high]:
+        arr[low], arr[high] = arr[high], arr[low]
 
-# # print( "# Quicksort 2")
-# # numbers = rnumbers.copy()
-# # for i in range(3):
-# #   start_time = time.time()
-# #   quickSort( numbers, 0, len(numbers)-1 )
-# #   end_time = time.time()
-# #   print(f"Time to sort {len(numbers)} elements: {end_time - start_time:.2f} seconds")
+    if arr[mid] > arr[high]:
+        arr[mid], arr[high] = arr[high], arr[mid]
+    
+    if (high - low) >= 2:
+        arr[mid], arr[high-1] = arr[high-1], arr[mid]
+
+    return low +1, high-1,  arr[high-1]
 
 
+def quickSortMediamOfThree(arr, low, high):
+    if (high <= low) : return
+
+    i, j, pivot = median_of_three(arr, low, high)     # pivot
+
+    while( i <= j ) :
+        if arr[i] < pivot:
+            i = i+1
+        else :
+            arr[i], arr[j] = arr[j], arr[i]
+            j = j-1
+    pi = i
+    arr[pi], arr[high] = arr[high], arr[pi]
+
+    quickSort(arr, low, pi-1)
+    quickSort(arr, pi+1, high)
+
+
+# timeMesure("# Quicksort original", quickSort)
+print(rnumbers)
+timeMesure("# Quicksort Mediam of three", quickSortMediamOfThree)
+# timeMesure("# Quicksort V3. Recursived", quickSortV3)
+# timeMesure("# Quicksort V. stacks", quickSortVStack)
+# timeMesure("# Quicksort V. stacks and Updated", quickSortV3VStack)
